161ec9eddd6096d13a9851faf1deccd1df5d1583
seabee33 <85791528+seabee33@users.noreply.github.com>
2023-12-25 23:34:29 +1100

    Update generateRandomString.php

--- generateRandomString.php
+++ generateRandomString.php
@@ -1,8 +1,7 @@
 <?php
+// openssl... is random binary garbage, looks like this (�5r�V�$L� /E1̨��Q ���C�^�)
+// Bin 2 Hex turns binary garbage to a readable string of random data like this (7234a94a6700f12d4a98109b3ca8a322619a4d435cf7eb6b4a7f5899f41a728d)
+
 $randomString = bin2hex(openssl_random_pseudo_bytes(32));
 echo $randomString;
 ?>
-
-
-// openssl... is random binary garbage, looks like this (�5r�V�$L� /E1̨��Q ���C�^�)
-// Bin 2 Hex turns binary garbage to a readable string of random data like this (7234a94a6700f12d4a98109b3ca8a322619a4d435cf7eb6b4a7f5899f41a728d)
